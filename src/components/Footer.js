75fa2abcf01fc2bb2027255457e7b8e450e55be1
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-05-14 15:24:43 +0200

    footer info updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,11 @@ import Container from 'react-bootstrap/Container';
 function Footer() {
   return (
     <Container fluid id='footer'>
-      <div className='copyright'>&copy; 2024 corporate. All rights reserved</div>
+      <div className='copyright'>&copy; 2024 talentcol. All rights reserved</div>
       <div className='socials'>
         <ul>
           <li>
             <a href='www.facebook.com'><i className='fab fa-facebook-f'></i></a>
-          </li>
-          <li>
-            <a href='www.facebook.com'><i className='fab fa-twitter'></i></a>
           </li>
           <li>
             <a href='www.facebook.com'><i className='fab fa-linkedin-in'></i></a>
